88cba4056aa85bcf31b26f72af2e86798476b184
Jahid0178 <jahidulhaque767@gmail.com>
2024-10-03 22:45:57 +0600

    fix: fixed featured section slider issue

--- src/components/Slider/FeatureSlider.tsx
+++ src/components/Slider/FeatureSlider.tsx
@@ -1,5 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCreative } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/effect-creative";
 import { features } from "../../data/data";
 import FeaturedCard from "../Cards/FeaturedCard/FeaturedCard";
 
@@ -12,14 +14,26 @@ const FeatureSlider = ({ swiperRef }: FeatureSliderProps) => {
   return (
     <Swiper
       onSwiper={(swiper) => (swiperRef.current = swiper)}
-      slidesPerView={1}
-      spaceBetween={10}
+      slidesPerView={3}
+      loop={true}
       breakpoints={{
         640: {
-          slidesPerView: 2,
-          spaceBetween: 10,
+          slidesPerView: 3,
         },
       }}
+      effect={"creative"}
+      creativeEffect={{
+        prev: {
+          shadow: true,
+          translate: ["490%", 0, -400],
+          opacity: 0.3,
+        },
+        next: {
+          translate: ["100%", 0, 0],
+        },
+        limitProgress: 4,
+      }}
+      modules={[EffectCreative]}
     >
       {features.map((feature) => (
         <SwiperSlide key={feature.id}>
